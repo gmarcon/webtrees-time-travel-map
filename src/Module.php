d69971e6e73f5645f50b6130a578165aab0cf199
Giulio Marcon <gmarcon@gmail.com>
2026-01-06 17:33:15 +0100

    version bump to 1.0.2

--- src/Module.php
+++ src/Module.php
@@ -52,7 +52,7 @@ class Module extends AbstractModule implements ModuleCustomInterface, ModuleChar
      */
     public function customModuleVersion(): string
     {
-        return '1.0.1';
+        return '1.0.2';
     }
 
     /**
